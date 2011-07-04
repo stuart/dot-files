8952499f42f4f8863c1fc8d6c44b3821b4cbb8da
Bodaniel Jeanes <me@bjeanes.com>
2011-07-04 10:42:28 +1000

    prompt.sh was not working in bash. TODO check for shell.

--- shell/common.sh
+++ shell/common.sh
@@ -5,7 +5,7 @@ source $DOT_FILES/$CURRENT_SHELL/config.*sh
 source $DOT_FILES/shell/exports.sh
 source $DOT_FILES/shell/aliases.sh
 source $DOT_FILES/shell/functions.sh
-source $DOT_FILES/shell/prompt.sh
+#source $DOT_FILES/shell/prompt.sh
 source $DOT_FILES/shell/git.sh
 
 [[ `uname -s` == 'Darwin' ]] && source $DOT_FILES/shell/osx.sh
